00597042c5c9222b40b6348dd0523c7c7fef7c5f
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2023-07-13 20:44:48 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world!!");
+    res.send("Hello I'm Terence! I've successfully deployed my code in heroku!");
 }
 
 module.exports = print;

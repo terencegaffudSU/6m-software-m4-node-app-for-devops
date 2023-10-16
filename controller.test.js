c98e04c8fbf228f1de0ded7a5baf757f769e374f
terencegaffudSU <terence.gaffud@skillsunion.com>
2023-10-16 20:57:22 +0800

    Added scan job in config.yml

--- controller.test.js
+++ controller.test.js
@@ -7,7 +7,7 @@ describe("the print function", ()=>{
         jest.clearAllMocks();
     })
 
-    it("should respond with 'Hello world!'", ()=>{
+    it("Request and response should be same", ()=>{ //test name
         const mockReq = {};
         const mockRes = {
             send:jest.fn()

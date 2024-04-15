016a3031ee5f73ed873a13e638493431ea0c411f
terencegaffudSU <terence.gaffud@skillsunion.com>
2024-04-15 19:13:47 +0800

    changed controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello I'm Terence! I've successfully deployed my code in heroku!");
+    res.send("Hello I'm Terence!");
 }
 
 module.exports = print;

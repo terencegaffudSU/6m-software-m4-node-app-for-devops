82d9460852683e32bb0e59270a862e971681353b
terencegaffudSU <terence.gaffud@skillsunion.com>
2025-02-06 14:13:43 +0800

    added render

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello World!!");
+    res.send("Hello World from Render");
 }
 
 module.exports = print;
